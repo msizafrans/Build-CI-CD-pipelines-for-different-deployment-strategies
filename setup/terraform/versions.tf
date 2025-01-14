05d56b7b60d2398bfe1604e8ea98e152cfde4c44
Frans <135562158+msizafrans@users.noreply.github.com>
2025-01-14 08:46:53 +0200

    Update versions.tf

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -11,3 +11,55 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = "1.4.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = "1.4.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = "1.4.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = "1.4.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
